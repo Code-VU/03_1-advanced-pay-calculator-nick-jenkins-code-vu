947733747d6004e9b168f9aa2e54befb3416f9c6
Nick Jenkins <Nick.Jenkins@vu.com>
2025-01-21 21:44:16 -0600

    Nick PayCalculator

--- payCalculator.py
+++ payCalculator.py
@@ -1,9 +1,24 @@
 def calculatePay():
     # This first line is provided for you
     hrs = input("Enter Hours:")
-    
+    pay = input("Enter Pay:")
     # end assignment
-
+    try:
+        h = float(hrs)
+        p = float(pay)
+    except:
+        print ("Error, please enter numeric input")
+        quit()
+    if h > 40 :
+        print("OverTime")
+        reg = h * p
+        opt = (h - 40.0) * (p *.5)
+        print(reg,opt)
+        x = reg + opt
+    else:
+        print("Regular") 
+        x = (h * p)
+    print("Pay:",x)
 ## if you want to test locally before you try to sync
 ## run > python payCalculator.py
 
